c412ba28d7834890e95acd6d38f451fba15a5d35
Noize Creations <45266268+Stereo-Noize@users.noreply.github.com>
2018-11-24 03:34:04 +0200

    update

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	
+	// Mobile Menu
+
+	$('.top-bar__menu__trigger').click(function() {
+		$('.top-bar__menu__list').toggleClass('top-bar__menu__list-open');
+	});
+
+
+});
